683dd582305593d818912089bfc1c3520a148d46
danielecioffo <d.cioffo@studenti.unipi.it>
2021-02-05 17:51:00 +0100

    New version of data.csv

--- FeatureExtractor/main.py
+++ FeatureExtractor/main.py
@@ -5,23 +5,26 @@ import matplotlib.pyplot as plt
 import os
 import pathlib
 import csv
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 ORIGINAL_DATASET_PATH = "genres/"
 genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
 
 
-# NOT USED, but i saw in the documentation of Librosa that spectrograms can be used to obtain more accurate data
-def extract_spectrograms():
+# NOT USED, but spectograms can be used to feed a Convolutional Neural Network, for better performance
+def extract_spectrograms_from_original_dataset():
     """
-    Function used for print all the spectrograms
+    Function used for create all the spectrograms
     """
-    cmap = plt.get_cmap('inferno')
+    cmap = plt.get_cmap('inferno')  # only for cosmetic reason
     plt.figure(figsize=(10, 10))
     for g in genres:
         pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)
         for filename in os.listdir(ORIGINAL_DATASET_PATH + f'{g}'):
             song_name = ORIGINAL_DATASET_PATH + f'{g}/{filename}'
-            y, sr = librosa.load(song_name, mono=True, duration=5)
+            y, sr = librosa.load(song_name, mono=True)
+            # Fs: sampling unit, Fc: center frequency
+            # Fs, Fc, noverlap, sides, mode are set to the default values
             plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default',
                          scale='dB')
             plt.axis('off')
@@ -53,7 +56,7 @@ def extract_features_from_original_dataset():
         for filename in os.listdir(ORIGINAL_DATASET_PATH + f'{g}'):
             song_name = ORIGINAL_DATASET_PATH + f'{g}/{filename}'
             # y is the floating point time series of the audio, sr is the sampling rate
-            y, sr = librosa.load(song_name, mono=True, duration=30)
+            y, sr = librosa.load(song_name, mono=True, duration=30) # duration 30s, the duration of the audio clip
             # Compute a chromagram
             chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
             # Compute root-mean-square (RMS) energy for each frame
@@ -79,8 +82,17 @@ def extract_features_from_original_dataset():
 def show_dataset():
     data = pd.read_csv('data.csv')
     print(data.head())
+    print("-------------------------")
+    # Other operations performed in the notebook
+    genre_list = data.iloc[:, -1]
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(genre_list)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype=float))
+    print(y)
+    print("-------------------------")
+    print(X)
 
 
 if __name__ == '__main__':
-    extract_features_from_original_dataset()
     show_dataset()
