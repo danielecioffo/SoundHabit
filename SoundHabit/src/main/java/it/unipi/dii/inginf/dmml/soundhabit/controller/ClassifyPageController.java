5c74896e72667249d9857145d1d2996cf3dd1035
fbaldi6 <f.baldi5@studenti.unipi.it>
2021-02-15 14:47:52 +0100

    Added chart

--- SoundHabit/src/main/java/it/unipi/dii/inginf/dmml/soundhabit/controller/ClassifyPageController.java
+++ SoundHabit/src/main/java/it/unipi/dii/inginf/dmml/soundhabit/controller/ClassifyPageController.java
@@ -5,24 +5,28 @@ import it.unipi.dii.inginf.dmml.soundhabit.classification.FeatureExtractor;
 import it.unipi.dii.inginf.dmml.soundhabit.classification.SongFeature;
 import it.unipi.dii.inginf.dmml.soundhabit.utils.Utils;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import javafx.util.Pair;
 
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ClassifyPageController {
+    @FXML public BarChart barChart;
     @FXML private TextField filePath;
     @FXML private Button selectFileButton;
     @FXML private Button classifyButton;
     @FXML private ProgressIndicator progressCircle;
     @FXML private Label waitLabel;
+
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     /**
      ** Method called when the controller is initialized
@@ -48,15 +52,33 @@ public class ClassifyPageController {
             return;
         }
 
-        waitLabel.setVisible(true);
+        clearScene();
         progressCircle.setVisible(true);
+        waitLabel.setVisible(true);
 
         Classifier classifier = Classifier.getInstance();
         FeatureExtractor f = new FeatureExtractor();
         SongFeature song = f.getSongFeaturesOfSong(filePath.getText());
-        int classify = classifier.classify(song.toInstances());
+        Pair<Integer, double[]> classify = classifier.classify(song.toInstances());
 
         progressCircle.setVisible(false);
-        waitLabel.setText("The genre is " + Utils.integerToGenre(classify));
+        waitLabel.setText("The genre is " + Utils.integerToGenre(classify.getKey()));
+        barChart.setVisible(true);
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("% Affinity");
+        series1.getData().add(new XYChart.Data("BLUES", classify.getValue()[0] * 100));
+        series1.getData().add(new XYChart.Data("CLASSICAL", classify.getValue()[1] * 100));
+        series1.getData().add(new XYChart.Data("JAZZ", classify.getValue()[2] * 100));
+        series1.getData().add(new XYChart.Data("METAL", classify.getValue()[3] * 100));
+        series1.getData().add(new XYChart.Data("POP", classify.getValue()[4] * 100));
+        series1.getData().add(new XYChart.Data("ROCK", classify.getValue()[5] * 100));
+        barChart.getData().addAll(series1);
+    }
+
+    private void clearScene() {
+        barChart.getData().clear();
+        barChart.setVisible(false);
+        waitLabel.setText("Please wait...");
     }
 }
