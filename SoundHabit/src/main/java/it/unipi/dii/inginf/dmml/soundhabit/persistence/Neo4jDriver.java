96b0a8bb9c45fb526883c7eb637011a20c23a6be
fbaldi6 <f.baldi5@studenti.unipi.it>
2021-02-16 09:39:43 +0100

    Added populateDatabase

--- SoundHabit/src/main/java/it/unipi/dii/inginf/dmml/soundhabit/persistence/Neo4jDriver.java
+++ SoundHabit/src/main/java/it/unipi/dii/inginf/dmml/soundhabit/persistence/Neo4jDriver.java
@@ -1,6 +1,7 @@
 package it.unipi.dii.inginf.dmml.soundhabit.persistence;
 
 import it.unipi.dii.inginf.dmml.soundhabit.config.ConfigurationParameters;
+import it.unipi.dii.inginf.dmml.soundhabit.model.Genre;
 import it.unipi.dii.inginf.dmml.soundhabit.model.Song;
 import it.unipi.dii.inginf.dmml.soundhabit.model.User;
 import it.unipi.dii.inginf.dmml.soundhabit.utils.Utils;
@@ -232,4 +233,77 @@ public class Neo4jDriver {
         }
     }
 
+    public void populateDatabase() {
+        // ROCK
+        addSong(new Song("Purple Haze", Genre.ROCK, "https://youtu.be/WGoDaYjdfSg", "Jimi Hendrix", "https://img.discogs.com/_PkIEYMnddZ2B5ORa5rCjiKB64c=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1249202-1203693818.jpeg.jpg"));
+        addSong(new Song("Under Pressure", Genre.ROCK, "https://youtu.be/a01QQZyl-_I", "Queen", "https://e-cdns-images.dzcdn.net/images/cover/e16455433a84c7e19025403ae3eec52d/350x350.jpg"));
+        addSong(new Song("Start Me Up", Genre.ROCK, "https://youtu.be/SGyOaCXr8Lw", "The Rolling Stones", "https://images-na.ssl-images-amazon.com/images/I/91Bl4KIUFUL._AC_SL1500_.jpg"));
+        addSong(new Song("Proud Mary", Genre.ROCK, "https://youtu.be/5hid10EgMXE", "Creedence Clearwater Revival", "https://upload.wikimedia.org/wikipedia/en/thumb/3/35/CCR_-_Proud_Mary.png/220px-CCR_-_Proud_Mary.png"));
+        addSong(new Song("Rockin’ in the Free World", Genre.ROCK, "https://youtu.be/TnAgc1kgvLc", "Neil Young", "https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Rockin_in_the_Free_World_single_cover.jpg/220px-Rockin_in_the_Free_World_single_cover.jpg"));
+        addSong(new Song("Whole Lotta Love", Genre.ROCK, "https://youtu.be/HQmmM_qwG4k", "Led Zeppelin", "https://www.debaser.it/resize.aspx?path=%2Ffiles%2F2016%2F44395.jpg"));
+        addSong(new Song("Should I Stay or Should I Go", Genre.ROCK, "https://youtu.be/BN1WwnEDWAM", "The Clash","https://static.wikia.nocookie.net/strangerthings8338/images/6/68/Combat_rock.jpg/revision/latest?cb=20170617174328"));
+        addSong(new Song("Walk This Way", Genre.ROCK, "https://youtu.be/4c8O2n1Gfto", "Aerosmith", "https://img.discogs.com/M7eH7TjqnLX8R-CIT-qZIY0HYAk=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2789979-1443013763-6843.jpeg.jpg"));
+        addSong(new Song("Baba O’Riley", Genre.ROCK, "https://youtu.be/gY5rztWa1TM", "The Who", "https://upload.wikimedia.org/wikipedia/en/5/5a/The_Who_-_Baba_cover.jpg"));
+        addSong(new Song("Comfortably Numb", Genre.ROCK, "https://youtu.be/x-xTttimcNk", "Pink Floyd", "https://i.ytimg.com/vi/_FrOQC-zEog/hqdefault.jpg"));
+
+        // METAL
+        addSong(new Song("Paranoid", Genre.METAL, "https://youtu.be/0qanF-91aJo", "Black Sabbath", "https://images-na.ssl-images-amazon.com/images/I/71vSn%2BlK4WL._AC_SL1425_.jpg"));
+        addSong(new Song("Master of Puppets", Genre.METAL, "https://youtu.be/u6LahTuw02c", "Metallica", "https://images-na.ssl-images-amazon.com/images/I/81hryXAVZjL._AC_SX522_.jpg"));
+        addSong(new Song("Ace of Spades", Genre.METAL, "https://youtu.be/3mbvWn1EY6g", "Motorhead", "https://www.nemesisnow.com/media/catalog/product/cache/7c3262bc18147ce5dc018ae87bc5e673/b/5/b5367s0.jpg"));
+        addSong(new Song("Crazy Train", Genre.METAL, "https://youtu.be/tMDFv5m18Pw", "Ozzy Osbourne", "https://img.discogs.com/8bUU93zXGsURUsyY-j4ZzEECB_4=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-9746157-1485711017-2680.jpeg.jpg"));
+        addSong(new Song("Welcome To The Jungle", Genre.METAL, "https://youtu.be/o1tj2zJ2Wvg", "Guns N' Roses", "https://upload.wikimedia.org/wikipedia/en/d/d6/Welcometothejungle.jpg"));
+        addSong(new Song("The Number of the Beast", Genre.METAL, "https://youtu.be/WxnN05vOuSM", "Iron Maiden", "https://img.discogs.com/bDcM6fLNCYutvHmREnGWpWw_4To=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6191421-1428850748-1506.jpeg.jpg"));
+        addSong(new Song("Hallowed Be Thy Name", Genre.METAL, "https://youtu.be/HAQQUDbuudY", "Iron Maiden", "https://m.media-amazon.com/images/I/81wajmBj+6L._SS500_.jpg"));
+        addSong(new Song("War Pigs", Genre.METAL, "https://youtu.be/iL23hA-RBz4", "Black Sabbath","https://www.debaser.it/files/2011%2F36069.jpeg"));
+        addSong(new Song("Back in Black", Genre.METAL, "https://youtu.be/pAgnJDJN4VA", "AC/DC", "https://upload.wikimedia.org/wikipedia/commons/b/be/Acdc_backinblack_cover.jpg"));
+        addSong(new Song("Iron Man", Genre.METAL, "https://youtu.be/5s7_WbiR79E", "Black Sabbath", "https://i.pinimg.com/originals/b0/cd/1b/b0cd1ba25db2030343bad6a0598c1ac6.jpg"));
+
+        // POP
+        addSong(new Song("Like a Prayer", Genre.POP, "https://youtu.be/79fzeNUqQbQ", "Madonna", "https://images-na.ssl-images-amazon.com/images/I/71WJtJ0Y3aL._AC_SX425_.jpg"));
+        addSong(new Song("When Doves Cry", Genre.POP, "https://youtu.be/UG3VcCAlUgE", "Prince", "https://i1.sndcdn.com/artworks-000430829199-eh8fy7-t500x500.jpg"));
+        addSong(new Song("I Wanna Dance With Somebody", Genre.POP, "https://youtu.be/eH3giaIzONA", "Whitney Houston", "https://m.media-amazon.com/images/I/71LuMhNwMBL._SS500_.jpg"));
+        addSong(new Song("Baby One More Time", Genre.POP, "https://youtu.be/C-u5WLJ9Yk4", "Britney Spears", "https://images.genius.com/661f50edcfefa2be10498a3b064dfada.1000x1000x1.jpg"));
+        addSong(new Song("It's Gonna Be Me", Genre.POP, "https://youtu.be/GQMlWwIXg3M", "'N Sync", "https://upload.wikimedia.org/wikipedia/en/f/f2/ItsGonnaBeMe.jpg"));
+        addSong(new Song("Everybody (Backstreet's Back", Genre.POP, "https://youtu.be/6M6samPEMpM", "Backstreet Boys", "https://img.discogs.com/Xg16mnU-q539cuXWMcG4amSFFKs=/fit-in/600x525/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-553783-1537901504-9791.jpeg.jpg"));
+        addSong(new Song("Rolling in the Deep", Genre.POP, "https://youtu.be/rYEDA3JcQqw", "Adele", "https://m.media-amazon.com/images/I/91CzVbxeUVL._SS500_.jpg"));
+        addSong(new Song("Don't Stop Believing", Genre.POP, "https://youtu.be/1k8craCGpgs", "Journey", "https://img.discogs.com/Abn91eeWzN38tKbPPcnEmYIlNuY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-419741-1454675353-7372.jpeg.jpg"));
+        addSong(new Song("Genie In a Bottle", Genre.POP, "https://youtu.be/kIDWgqDBNXA", "Christina Aguilera", "https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Genie_in_a_Bottle.png/220px-Genie_in_a_Bottle.png"));
+        addSong(new Song("Shake It Off", Genre.POP, "https://youtu.be/nfWlot6h_JM", "Taylor Swift", "https://hooksandharmony.com/wp-content/uploads/2014/12/Taylor-Swift-Shake-It-Off-e1534088206551.jpg"));
+
+        // BLUES
+        addSong(new Song("I Can't Quit You Baby", Genre.BLUES, "https://youtu.be/Uy2tEP3I3DM", "Otis Rush", "https://img.discogs.com/UdK54nheZ7ZNllIpXUm7pEXpMwo=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-11501352-1517468434-9815.jpeg.jpg"));
+        addSong(new Song("I'd Rather Go Blind", Genre.BLUES, "https://youtu.be/u9sq3ME0JHQ", "Etta James", "https://img.discogs.com/5iXl17TSCYxw37aux2POT95r2lM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-5759576-1401882130-9427.jpeg.jpg"));
+        addSong(new Song("Crossroad Blues", Genre.BLUES, "https://youtu.be/Yd60nI4sa9A", "Robert Johnson", "https://i.ytimg.com/vi/GtDlZdhHRCI/hqdefault.jpg"));
+        addSong(new Song("Pride and Joy", Genre.BLUES, "https://youtu.be/0vo23H9J8o8", "Stevie Ray Vaughan", "https://img.discogs.com/9YfJ71TFWGA6RcfhqHeku3zZgA4=/fit-in/600x578/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5566914-1422280383-3583.jpeg.jpg"));
+        addSong(new Song("I'm Tore Down", Genre.BLUES, "https://youtu.be/YB52eLfirFA", "Freddie King", "https://images-na.ssl-images-amazon.com/images/I/51zPOmBtU5L._AC_SY355_.jpg"));
+        addSong(new Song("Born Under a Bad Sign", Genre.BLUES, "https://youtu.be/2Py37G9qsfY", "Albert King", "https://images-na.ssl-images-amazon.com/images/I/71wrBTBEVgL._AC_SL1400_.jpg"));
+        addSong(new Song("Sunshine of your Love", Genre.BLUES, "https://youtu.be/y_u1eu6Lpds", "Cream", "http://static1.squarespace.com/static/572a4d599f7266c0b7e6632c/59e67c6101002744ef9cbd42/59e67f9fb078695e0a004eea/1508371483289/03330327e03d85efcd0f90d54b6d7851.998x1000x1.jpg?format=1500w"));
+        addSong(new Song("Hoochie Coochie Man", Genre.BLUES, "https://youtu.be/U5QKpsVzndc", "Muddy Waters", "https://images-na.ssl-images-amazon.com/images/I/71Eliheb7bL._AC_SY355_.jpg"));
+        addSong(new Song("Red House", Genre.BLUES, "https://youtu.be/G2SlSokLN4c", "Jimi Hendrix", "https://images-na.ssl-images-amazon.com/images/I/51kuUityivL._AC_SY355_.jpg"));
+        addSong(new Song("The Thrill is Gone", Genre.BLUES, "https://youtu.be/kpC69qIe02E", "B.B. King", "https://m.media-amazon.com/images/I/61u5jrBN1jL._SS500_.jpg"));
+
+        // JAZZ
+        addSong(new Song("So What", Genre.JAZZ, "https://youtu.be/ylXk1LBvIqU", "Miles Davis", "https://images-na.ssl-images-amazon.com/images/I/713VRATpUtL._AC_SL1227_.jpg"));
+        addSong(new Song("Fly Me To The Moon", Genre.JAZZ, "https://youtu.be/ZEcqHA7dbwM", "Frank Sinatra", "https://static.qobuz.com/images/covers/9b/u4/qe9o9pdvuu49b_600.jpg"));
+        addSong(new Song("Mood Indigo", Genre.JAZZ, "https://youtu.be/bZyVBVFnrm4", "Duke Ellington", "https://img.discogs.com/uxpumP8pFHjVSbQXEwPiWMTrcmY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-6347836-1417031363-2628.jpeg.jpg"));
+        addSong(new Song("Take Five", Genre.JAZZ, "https://youtu.be/vmDDOFXSgAs", "Dave Brubeck Quartet", "https://images-na.ssl-images-amazon.com/images/I/71lsG3fCaOL._SL1200_.jpg"));
+        addSong(new Song("The Girl From Ipanema", Genre.JAZZ, "https://youtu.be/j8VPmtyLqSY", "Stan Getz", "https://img.discogs.com/Ds_Q2CL5v0s585K0jjIb3qMTIaM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-7575968-1444386356-9111.jpeg.jpg"));
+        addSong(new Song("Minnie the Moocher", Genre.JAZZ, "https://youtu.be/zZ5gCGJorKk", "Cab Calloway", "https://images-na.ssl-images-amazon.com/images/I/41ZTZSVYF6L._AC_.jpg"));
+        addSong(new Song("What a Wonderful World", Genre.JAZZ, "https://youtu.be/CWzrABouyeE", "Louis Armstrong", "https://images-na.ssl-images-amazon.com/images/I/71oBdOk%2B2BL._AC_SL1099_.jpg"));
+        addSong(new Song("Strange Fruit", Genre.JAZZ, "https://youtu.be/Web007rzSOI", "Billie Holiday", "https://images-na.ssl-images-amazon.com/images/I/71SsPaS6-9L._AC_SY355_.jpg"));
+        addSong(new Song("Georgia on my Mind", Genre.JAZZ, "https://youtu.be/QL3EZwSJAh0", "Ray Charles", "https://images-na.ssl-images-amazon.com/images/I/71N2xzDmgPL._AC_SL1083_.jpg"));
+        addSong(new Song("My Baby Just Cares for Me", Genre.JAZZ, "https://youtu.be/3ZS7iKdRo5Q", "Nina Simone", "https://images-na.ssl-images-amazon.com/images/I/81X6wONNRtL._AC_SL1500_.jpg"));
+
+        // CLASSICAL
+        addSong(new Song("Eine kleine Nachtmusik", Genre.CLASSICAL, "https://youtu.be/oy2zDJPIgwc", "Mozart", "https://www.slowfood.it/wp-content/uploads/2016/08/mozart-480x360.jpeg"));
+        addSong(new Song("Für Elise", Genre.CLASSICAL, "https://youtu.be/_mVW8tgGY_w", "Beethoven", "https://upload.wikimedia.org/wikipedia/commons/c/c0/Beethovensmall.jpg"));
+        addSong(new Song("O mio babbino caro", Genre.CLASSICAL, "https://youtu.be/0Wlcr2bIKmk", "Puccini", "https://i2.wp.com/www.gbopera.it/wp-content/uploads/2019/02/Acculturarsi_-Michele-Placido-d%C3%A0-voce-a-Puccini-tra-parole-e-mus_.jpg?fit=658%2C718&ssl=1"));
+        addSong(new Song("Toccata and Fugue in D Minor", Genre.CLASSICAL, "https://youtu.be/Nnuq9PXbywA", "J.S. Bach", "https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg"));
+        addSong(new Song("Symphony No.5 in C minor", Genre.CLASSICAL, "https://youtu.be/fOk8Tm815lE", "Beethoven", "https://upload.wikimedia.org/wikipedia/commons/c/c0/Beethovensmall.jpg"));
+        addSong(new Song("The Four Seasons", Genre.CLASSICAL, "https://youtu.be/GRxofEmo3HA", "Vivaldi", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Vivaldi.jpg/220px-Vivaldi.jpg"));
+        addSong(new Song("Carmen", Genre.CLASSICAL, "https://youtu.be/pmuFOuh3QHs", "Bizet", "https://upload.wikimedia.org/wikipedia/commons/9/96/Georges_bizet.jpg"));
+        addSong(new Song("The Blue Danube", Genre.CLASSICAL, "https://youtu.be/_CTYymbbEL4", "Johann Strauss II", "https://pociopocio.altervista.org/wp-content/uploads/2021/01/STRAUSS-II.2.jpg"));
+        addSong(new Song("Boléro", Genre.CLASSICAL, "https://youtu.be/r30D3SW4OVw", "Ravel", "https://biografieonline.it/img/bio/m/Maurice_Ravel.jpg"));
+        addSong(new Song("Flower Duet", Genre.CLASSICAL, "https://youtu.be/8Qx2lMaMsl8", "Delibes", "https://upload.wikimedia.org/wikipedia/commons/e/e2/L%C3%A9o_Delibes.jpg"));
+    }
 }
